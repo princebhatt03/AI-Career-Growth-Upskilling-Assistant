8c496eee8166623d7bde199b20791774a2f72daf
Prince Bhatt <princebhatt316@gmail.com>
2025-11-10 08:49:59 +0000

    Project Started, AI-Career-Growth-Upskilling-Assistant

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import ErrorPage from './pages/ErrorPage';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* Routes */}
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="*"
+            element={<ErrorPage />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="bg-red-600 text-white p-4">About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
